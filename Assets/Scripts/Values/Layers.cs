407007e0e185540c4187ed2729cd99f2718df8d0
nelu572 <s25031@gsm.hs.kr>
2026-09-25 03:53:30 +0900

    chore(Build): BackGround 레이어 등록

--- Assets/Scripts/Values/Layers.cs
+++ Assets/Scripts/Values/Layers.cs
@@ -3,4 +3,5 @@ public static class Layers
     public const string Player = "Player";
     public const string Environment = "Environment";
     public const string Interactable = "Interactable";
+    public const string BackGround = "BackGround";
 }
